804d40a4f65909fbb2394c083650dd8c9d4a1440
Moeinh77 <moeinhasani77@gmail.com>
2018-06-01 11:01:27 -0700

    sort added

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from collections import namedtuple
 
-Item = namedtuple("Weight_Value", "Weight Value isFractional")
+Item = namedtuple("Item", "Weight Value isFractional")
 
 InFile=open("IN.TXT","r+")
 
 lines=InFile.readlines()
 
+Items_list=[]
+
 for line in lines:
     i=line.split()
-    m=Item(i[0],i[1],i[2])
-    print(m)
+    m=Item(int(i[0]),int(i[1]),int(i[2]))
+    Items_list.append(m)
+
+#print(Items_list)
+
+
+def cmpfunc(ItemX):
+    return ItemX.Value/ItemX.Weight
+
+def KnapSack(W,list,N):
+    Items_list.sort(key=cmpfunc, reverse=True)
+
+
+
+print(Items_list)

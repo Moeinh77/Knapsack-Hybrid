0c61028e397e0dd3e33820fb8a0e03d8d7b3de64
Moeinh77 <moeinhasani77@gmail.com>
2018-06-13 10:48:31 -0700

    completed

--- main.py
+++ main.py
@@ -2,47 +2,71 @@ from collections import namedtuple
 
 Item = namedtuple("Item", "Weight Value isFractional")
 
-InFile=open("IN.TXT","r+")
+with open("IN.TXT", "r") as InFile:
+    lines = InFile.readlines()
 
-lines=InFile.readlines()
-
-Items_list=[]
+Items_list = []
+KnapSack_list=[]
+fractionated=None
 
 for line in lines:
-    i=line.split()
-    m=Item(int(i[0]),int(i[1]),int(i[2]))
+    i = line.split()
+    m = Item(int(i[0]), int(i[1]), int(i[2]))
     Items_list.append(m)
 
-
+print("\n***Items available : ")
 print(Items_list)
 
+
 def cmpfunc(ItemX):
-    return ItemX.Value/ItemX.Weight
+    return ItemX.Value / ItemX.Weight
 
-def KnapSack(W,list,N):
 
+def KnapSack(W, list, N):
+    global fractionated
     Items_list.sort(key=cmpfunc, reverse=True)
-    currentValue=0
-    remainedWeight=W
 
-    for i in range(0,N):
+    # print("\nList sorted by value per weight:")
+    # print(Items_list)
 
-        if(list[i].Weight<=remainedWeight):
+    currentValue = 0
+    remainedWeight = W
 
-            currentValue+=list[i].Value
-            remainedWeight-=list[i].Weight
+    for i in range(0, N):
+
+        if (list[i].Weight <= remainedWeight):
+
+            currentValue += list[i].Value
+            remainedWeight -= list[i].Weight
+            KnapSack_list.append(list[i])
 
         else:
 
-            if(list[i].isFractional==1):
-                currentValue+=(list[i].Value/list[i].Weight)*remainedWeight
+            if remainedWeight == 0:
+                break
+
+            if list[i].isFractional == 1:
+
+                currentValue += (list[i].Value / list[i].Weight) * remainedWeight
+                KnapSack_list.append(list[i])
+                fractionated=i
                 break
 
             else:
                 continue
 
-
     return currentValue
 
 
-print("\nMax value: "+str(KnapSack(3,Items_list,len(Items_list))))
+W = int(input("\nEnter the MAXIMUM capacity of the KNAPSACK: "))
+
+
+Maxval=KnapSack(W, Items_list, len(Items_list))
+
+print("\n***Taken items:")
+print(KnapSack_list)
+
+if(fractionated!=None):
+    print("\n"+str(Items_list[fractionated])+ " is Fractionated!!!")
+
+print("\nMax value that can be collected: " + str(Maxval))
